860618c1c0b36751c530b2e9c234bcd9e023c765
siverNa <69504543+siverNa@users.noreply.github.com>
2022-11-25 20:15:49 +0900

    Feat: 추가 hello.blade.php
    - 라라벨 실습을 위해 hello.blade.php 파일 추가

---
+++ laravel_tutorial/resources/views/hello.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>hello</title>
+</head>
+<body>
+    <p>라라벨 블레이드 연습 페이지입니다!</p>
+</body>
+</html>
